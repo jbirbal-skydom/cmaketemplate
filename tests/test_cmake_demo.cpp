dd92ff686abdbe427bbe809c487c8f6fe43fd970
skydom <github@skydomsolutions.com>
2023-03-29 18:57:52 +0300

    Smoother readability

--- tests/test_cmake_demo.cpp
+++ tests/test_cmake_demo.cpp
@@ -4,15 +4,7 @@
 
 int main() {
     printf("testing:");
-    char* req;
+    char *req;
     req = echo();
-    if (strcmp(req, "hello") == 0)
-    {
-        return 0;
-    } else
-    {
-        return 1;
-    }
-
-
+    return strcmp(req, "hello");
 }
